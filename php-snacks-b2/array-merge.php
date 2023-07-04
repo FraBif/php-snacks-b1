caac3361fcd647d248c222ba31c96d0f6083bb90
FraBif <francescobif@hotmail.it>
2023-07-04 12:26:19 +0200

    not sure

--- php-snacks-b2/array-merge.php
+++ php-snacks-b2/array-merge.php
@@ -1,7 +1,13 @@
 <?php
 
-$letters = ["A" , "B" , "C"]
+$letters = ["A" , "B" , "C"] ;
 
-$numbers = [1 , 2 , 3]
+$numbers = [1 , 2 , 3] ;
 
+function mergeArrays() {
+    $result = array_merge($letters, $numbers);
+    echo $result
+} ;
+
+mergeArrays()
 ?>
